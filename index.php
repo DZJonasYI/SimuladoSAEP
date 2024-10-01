de23819fe959523e81bfcc9b48f6897f26cfe3df
Jonas_F_M <126809250+DZJonasYI@users.noreply.github.com>
2024-10-01 08:22:22 -0300

    criando o site

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simulado Prático DS</title>
+</head>
+<body>
+
+    <div id="Login">
+        <h1>Bem vindo</h1>
+
+        <div class="email">
+
+            <form method="post" action="">
+
+            <input type="email" id="email" name="email">
+
+        </div>
+
+        <br>
+             <label for="Idade">Idade:</label>
+
+        <input type="password" id="senha" name="senha">
+
+        <br><br>
+
+        <input type="submit" value="Entrar">
+
+    </form>
+    </div>
+</body>
+</html>
